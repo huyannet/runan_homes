65e35a97dff925a866e6f5b6d8f5bc0d140c1e98
huyannet <huyannet@gmail.com>
2016-03-28 16:09:25 +0700

    Added files via upload

---
+++ custom.js
@@ -0,0 +1,24 @@
+/* Write here your custom javascript codes */
+function changeLocation(location) {
+    $.get("/getDistrict?location=" + location + "&cDistrict=" + cDistrict, function (data) {
+        $("#QuanHuyenIDValue").html(data);
+    });
+}
+
+jQuery(document).ready(function () {
+    //Search
+    $("#fulltextSearch").submit(function () {
+        var quanHuyenValue = $("#QuanHuyenIDValue").val();
+        var tinhThanhValue = $("#TinhThanhIDValue").val();
+        if (quanHuyenValue != null && quanHuyenValue != '') {
+            $("#location").val(quanHuyenValue);
+        } else {
+            $("#location").val(tinhThanhValue);
+        }
+        return true;
+    });
+
+    $(document).ready(function () {
+        $("#TinhThanhIDValue").change();
+    });
+});
